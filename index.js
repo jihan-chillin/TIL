8d770e150218587df37f036b7fe9c2738e6bdbd1
chilln <kimjihan77@naver.com>
2024-04-29 16:10:55 +0900

    STEP3:Integraing Socket.IO

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express")
 const { createServer } = require("node:http");
-const { join } = require("node:path")
+const path = require("node:path");
+const { join } = require("node:path");
+const { disconnect } = require("node:process");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server)
 
 app.get("/", (req, res) => {
     res.sendFile(join(__dirname, "index.html"))
+})
+
+io.on("connection", (socket) => {
+    console.log("user connected!!")
 })
 
 server.listen(3000, () => {
